2495abd273269937c198e69e0faef6011438c36d
Krystsina Bahdanava <Krystsina_Bahdanava@epam.com>
2020-06-08 14:45:18 +0300

    Added removal of files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import glob, os
+import shutil
 
 
 def change_dir():
@@ -6,15 +7,25 @@ def change_dir():
 
 
 def fb2_file_search():
+    incorrect_file_removal()
     fb2_file_list = {file for file in glob.glob("*.fb2")}
     return fb2_file_list
 
 
-def incorrect_file_search():
+def incorrect_file_removal():
+    change_dir()
     file_list = {file for file in glob.glob("*.*") if file not in glob.glob("*.fb2")}
-    return file_list
+    if not os.path.exists('incorrect_input'):
+        os.makedirs('incorrect_input')
+    for f in file_list:
+        shutil.move(f, 'incorrect_input')
 
 
-change_dir()
-print(fb2_file_search())
-print(incorrect_file_search())
+fb2_file_search()
+
+try:
+  print(x)
+except:
+  print("An exception occurred")
+
+
