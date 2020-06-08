684ab4082eb817cdf0fd78c6ff07d9335b21e110
Krystsina Bahdanava <Krystsina_Bahdanava@epam.com>
2020-06-08 12:57:31 +0300

    Check incorrect input files

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import glob, os
-os.chdir('./res')
-for file in glob.glob("*.fb2"):
-    print(file)
+
+
+def change_dir():
+    os.chdir('./res')
+
+
+def fb2_file_search():
+    fb2_file_list = {file for file in glob.glob("*.fb2")}
+    return fb2_file_list
+
+
+def incorrect_file_search():
+    file_list = {file for file in glob.glob("*.*") if file not in glob.glob("*.fb2")}
+    return file_list
+
+
+change_dir()
+print(fb2_file_search())
+print(incorrect_file_search())
