175136ca62260be4b4f5099ea4e1155fb0e9859a
Krystsina Bahdanava <Krystsina_Bahdanava@epam.com>
2020-06-09 14:24:11 +0300

    Count words method added

--- main.py
+++ main.py
@@ -25,20 +25,44 @@ def incorrect_file_removal():
 def count_paragraph(elem_tree):
     i = 0
     for section in elem_tree.iter('{http://www.gribuser.ru/xml/fictionbook/2.0}section'):
-        for _ in section.findall('{http://www.gribuser.ru/xml/fictionbook/2.0}p'):
+        for paragraph in section.findall('{http://www.gribuser.ru/xml/fictionbook/2.0}p'):
             i = i+1
-    print(i)
     return i
 
 
-change_dir()
-fb2_file_search()
-for fb2_file in fb2_file_search():
-    tree = ET.parse(fb2_file)
-    root = tree.getroot()
-    count_paragraph(tree)
+def count_words(elem_tree):
+    cnt_words = 0
+    for section in elem_tree.iter('{http://www.gribuser.ru/xml/fictionbook/2.0}section'):
+        for paragraph in section.findall('{http://www.gribuser.ru/xml/fictionbook/2.0}p'):
+            if (paragraph.text is None) or (len(paragraph.text) == 0):
+                continue
+            cnt_words = cnt_words + len(paragraph.text.split())
+    return cnt_words
 
 
+def get_book_name(elem_tree):
+    book_name = ""
+    for section in elem_tree.iter('{http://www.gribuser.ru/xml/fictionbook/2.0}title-info'):
+        for title in section.findall('{http://www.gribuser.ru/xml/fictionbook/2.0}book-title'):
+            book_name = title.text
+
+    return book_name
+
+
+def main():
+    change_dir()
+    fb2_file_search()
+    for fb2_file in fb2_file_search():
+        tree = ET.parse(fb2_file)
+        paragraph_cnt = count_paragraph(tree)
+        book_name = get_book_name(tree)
+        word_count = count_words(tree)
+        print(paragraph_cnt)
+        print(book_name)
+        print(word_count)
+
+
+main()
 
 
 """
